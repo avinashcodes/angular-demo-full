788aa48650b44bca64d81db33c77213c569925f2
AVINASH ANUPOJU <AANUPOJU@MICRON.COM>
2020-08-09 18:37:03 +0530

    Created dummy service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule, MatButtonModule, MatChipsModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatCardModule,
     MatButtonModule,
     MatChipsModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
